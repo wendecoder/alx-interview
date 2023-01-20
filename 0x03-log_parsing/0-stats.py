26ea2554a580264c8eb9262f1c235580b2b3211c
wendecoder <wenonezra258@gmail.com>
2023-01-20 03:51:40 +0300

    added 0-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,73 +1,40 @@
 #!/usr/bin/python3
-""" A script that reads stdin line by line
-and computer metrics
-"""
-import sys
+"""Performs log parsing from stdin"""
+
 import re
-import signal
-from collections import OrderedDict
+import sys
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def search_items(line, s):
-    """ search items to position"""
-    regexu = r"\s\d{3}\s\d{1,}"
-    txt = re.search(regexu, line)
-    word = txt.group()
-    word = word[1:]
-
-    regexd = r"\d{3}\s"
-    left = re.search(regexd, word)
-
-    code = left.group()
-    code = code[:-1]
-
-    regext = r"\s\d{1,}"
-    right = re.search(regext, word)
-
-    size = right.group()
-    size = size[1:]
-    size = int(size)
-
-    add_code(code, s)
-
-    return size
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
-def add_code(code, codes):
-    """count the status code"""
+if __name__ == "__main__":
     try:
-        codes[code] += 1
-    except KeyError:
-        pass
-
-
-def print_all(stat):
-    """ Print all """
-    stat = OrderedDict(stat)
-
-    for key, values in stat.items():
-        if value is not 0:
-            print("{}: {}".format(key, value))
-
-
-if __name__ == "__main_":
-    status = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    i = 0
-
-    try:
-        for lines in sys.stdin:
-            file_size += search_items(lines, status)
-
-            if i is not 0 and i % 9 == 0:
-                print("Files size: {:d}".format(file_size))
-                print_all(status)
-
-            i += 1
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except Exception:
+                pass
+        printCodes(statusC_counter, file_size)
     except KeyboardInterrupt:
-        pass
-    finally:
-        print("File size: {:d}".format(file_size))
-        print_all(status)
-        sys.exit(0)
+        printCodes(statusC_counter, file_size)
+        raise
